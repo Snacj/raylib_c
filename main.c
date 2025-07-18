3b92a7619b34f17316990f4a1fdd0e31a4bd7edb
snacj <0xsnacj@proton.me>
2025-07-18 23:20:45 +0200

    Testing Mouse input

--- main.c
+++ main.c
@@ -1,24 +1,57 @@
 #include "raylib.h"
 
-int main() {
-        const int screenWidth = 640;
-        const int screenHeight = 360;
+int main(void)
+{
+    // Initialization
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 450;
 
-        InitWindow(screenWidth, screenHeight, "raylib");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib");
 
-        SetTargetFPS(60);
+    Vector2 ball_position = { -100.0f, -100.0f };
+    Color ball_color = DARKBLUE;
+    int isCursorHidden = 0;
 
-        while (!WindowShouldClose())
-        {
-                BeginDrawing();
-                        ClearBackground(RAYWHITE);
+    SetTargetFPS(60);
 
-                        DrawText(" Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-                EndDrawing();
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // update
+        if (IsKeyPressed(KEY_H)) {
+            HideCursor();
+            isCursorHidden = 1;
+        } else {
+            ShowCursor();
+            isCursorHidden = 0;
         }
 
-        CloseWindow();
+        ball_position = GetMousePosition();
 
-        return 0;
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) ball_color = MAROON;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ball_color = LIME;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ball_color = DARKBLUE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_SIDE)) ball_color = PURPLE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ball_color = YELLOW;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ball_color = ORANGE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ball_color = BEIGE;
+
+        // Draw
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawCircleV(ball_position, 40, ball_color);
+
+            DrawText("move ball with mouse and click mouse to change color", 10, 10, 20, DARKGRAY);
+            DrawText("Press 'H' to toggle cursor visibility", 10, 30, 20, RED);
+
+            if (isCursorHidden == 1) DrawText("CURSOR HIDDEN", 20, 60, 20, RED);
+            else DrawText("CURSOR VISIBLE", 20 , 60, 20, LIME);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();
+    return 0;
 }
-
